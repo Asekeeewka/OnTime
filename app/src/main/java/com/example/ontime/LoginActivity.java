130d108e81accc1b37bb4b14602f7a7b469f8292
Askar Aytkazin <neitral2011@gmail.com>
2020-04-20 16:29:32 +0600

    Login and Reg Activities properly working

--- app/src/main/java/com/example/ontime/LoginActivity.java
+++ app/src/main/java/com/example/ontime/LoginActivity.java
@@ -2,13 +2,52 @@ package com.example.ontime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView login_to_register;
+    private Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        login_to_register = (TextView) findViewById(R.id.login_page_reg);
+        login_button = (Button) findViewById(R.id.login_page_login_button);
+
+
+
+        login_to_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, RestoranActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                startActivity(i);
+                finish();
+
+
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/ontime/RegistrationActivity.java
+++ app/src/main/java/com/example/ontime/RegistrationActivity.java
@@ -2,13 +2,45 @@ package com.example.ontime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+
+    private TextView register_to_login;
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+
+        register_to_login = (TextView) findViewById(R.id.register_page_log);
+        register = (Button) findViewById(R.id.register_page_register_button);
+
+        register_to_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
